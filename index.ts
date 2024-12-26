3785946c5beef76b8ce97726c253c4e7179ef5e6
Erdem <zahrakon06@gmail.com>
2024-12-27 01:20:04 +0300

    .

--- index.ts
+++ index.ts
@@ -2,10 +2,17 @@ import "./env";
 import fs from "fs";
 import { addKeyword, addTranslation, find, resetCollection } from "./collection";
 import { translate } from "./api";
+import yaml from "yaml";
 
 let output = {};
 
+const onlyKeys = process.env.ONLY_KEYS?.split(",").map(e => e.trim()) || [];
+
 async function main() {
+  if (fs.existsSync("./input.yaml")) {
+    const input = yaml.parse(await fs.promises.readFile("./input.yaml", "utf-8"));
+    await fs.promises.writeFile("./input.json", JSON.stringify(input, null, 2));
+  }
   const input = JSON.parse(await fs.promises.readFile("./input.json", "utf-8"));
 
   await resetCollection();
@@ -13,15 +20,27 @@ async function main() {
   output = await translateRecursive(input, undefined, []);
 
   await fs.promises.writeFile("./output.json", JSON.stringify(output, null, 2));
+  if (fs.existsSync("./input.yaml")) {
+    await fs.promises.writeFile("./output.yaml", yaml.stringify(output));
+  }
 }
 
 async function translateRecursive(input: any, key?: string, recursiveKeys: string[] = []) {
   if (typeof input === "string") {
+    if ((process.env.ONLY_KEYS?.length || 0) > 0) {
+      if (!key) return input;
+      if (!onlyKeys.includes(key)) return input;
+    }
     return await translateString(input, key!);
   } else if (Array.isArray(input)) {
     if (input.every(e => typeof e === "string")) {
+      if ((process.env.ONLY_KEYS?.length || 0) > 0) {
+        if (!key) return input;
+        if (!onlyKeys.includes(key)) return input;
+      }
       const fullText = input.join("\\n");
-      return (await translateString(fullText, key!)).split(/\n|\\n/);
+      const resultText = (await translateString(fullText, key!));
+      return ((resultText as any)?.[key as any] || resultText).split(/\n|\\n/);
     }
     return input;
   } else if (typeof input === "object") {
